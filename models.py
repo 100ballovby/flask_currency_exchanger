2cec948ac8b947160c02992f7494ee67cef9b611
demidraksin <greatraksin@icloud.com>
2021-05-14 13:05:34 +0300

    Adding new table to the database⭐️

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
 from app import db
+
+
+class Currencies(db.Model):
+    '''Таблица для хранения валют и кодов валют'''
+    __tablename__ = 'currencies'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(3), unique=True)
+    name = db.Column(db.String(200), unique=True)
+
+    def __repr__(self):
+        return f'Code: {self.code}, name: {self.name}'
